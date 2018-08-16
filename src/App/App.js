de7edd5bb5feacff6c554c84635ab0f4e38897ec
Mateusz Solecki <solmat@gmail.com>
2018-08-16 23:54:28 +0200

    prepare list of items

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,76 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import { Button, Card, CardActions, CardContent, Grid, Typography } from '@material-ui/core'
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+})
 
 class App extends Component {
   render () {
+    const { classes } = this.props
+    const issues = [
+      {
+        id: 0,
+        title: 'test Title',
+        desc: 'desc'
+      },
+      {
+        id: 1,
+        title: 'test Title 2',
+        desc: 'desc'
+      },
+      {
+        id: 2,
+        title: 'test Title 3',
+        desc: 'desc'
+      }
+    ]
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={classNames(classes.layout, classes.cardGrid)}>
+        <Grid container spacing={40}>
+          {issues.map(issue => (
+            <Grid item key={issue.id} sm={12}>
+              <Card>
+                <CardContent>
+                  <Typography gutterBottom variant="headline" component="h3">
+                    {issue.title}
+                  </Typography>
+                  <Typography>
+                    {issue.desc}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     )
   }
 }
 
-export default App
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(App)
