e20ef9e9d411d22c20aa62622c82ac686a7fa01d
Mateusz Solecki <solmat@gmail.com>
2018-08-17 02:30:38 +0200

    use shallow rendering to test main component

---
+++ src/App/index.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import App from './index'
+import { shallow } from 'enzyme'
+import { Grid } from '@material-ui/core'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+describe('AppComponent', () => {
+  const mockAdapter = new MockAdapter(axios)
+
+  beforeAll(() => {
+    mockAdapter.onGet('https://api.github.com/repos/facebook/react/issues?state=all&sort=updated').reply(200)
+  })
+
+  afterAll(() => {
+    mockAdapter.reset()
+  })
+
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />).dive()
+
+    console.log(wrapper)
+
+    expect(wrapper.find(Grid).length).toEqual(1)
+  })
+})
--- src/GithubClient.js
+++ src/GithubClient.js
@@ -10,17 +10,21 @@ export default class GithubClient {
   getIssues = async () => {
     const response = await this.client.get('/issues?state=all&sort=updated')
 
-    return response.data.map(issue => ({
-      id: issue.id,
-      title: issue.title,
-      state: issue.state,
-      createdAt: (new Date(issue.created_at)).toDateString(),
-      description: issue.body,
-      user: {
-        login: issue.user.login,
-        url: issue.user.url,
-        avatar: issue.user.avatar_url
-      }
-    }))
+    if (response.data) {
+      return response.data.map(issue => ({
+        id: issue.id,
+        title: issue.title,
+        state: issue.state,
+        createdAt: (new Date(issue.created_at)).toDateString(),
+        description: issue.body,
+        user: {
+          login: issue.user.login,
+          url: issue.user.url,
+          avatar: issue.user.avatar_url
+        }
+      }))
+    }
+
+    return []
   }
 }
